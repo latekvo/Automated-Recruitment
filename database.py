6fc4c2f78b359f1f0dc47328136bde3f5bc73e4c
Ignacy Łątka <latkaignacy@gmail.com>
2024-06-25 06:26:53 +0200

    move schema to db, use sqlite

--- database.py
+++ database.py
@@ -4,7 +4,34 @@ from sqlalchemy.orm import Mapped, mapped_column, Session, relationship, Declara
 class Base(DeclarativeBase):
   pass
 
-engine = create_engine()
+class Submission(Base):
+  # single sent answer to Task or TechnicalTask
+  pass
 
-def init_database():
-  Base.metadata.create_all(bind=engine)
+class PersonalData(Base):
+  # CV, and required extracted data
+  pass
+
+class Application(Base):
+  # applicant's work results
+  pass
+
+class Task(Base):
+  # question which may be given to a candidate
+  pass
+
+class TechnicalTask(Base):
+  # question with a coding task
+  pass
+
+class Recruitment(Base):
+  # single position to which you might recruit someone
+  pass
+
+class Platform(Base):
+  # platform details on which a batch was deployed
+  pass
+
+
+engine = create_engine("sqlite://")
+Base.metadata.create_all(bind=engine)
