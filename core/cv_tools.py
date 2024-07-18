dbb6c38ce9a2465406282feaee7ee2563784d259
LatekVon <latkaignacy@gmail.com>
2024-07-19 00:32:47 +0200

    expand tools implementation, update tests

--- core/cv_tools.py
+++ core/cv_tools.py
@@ -1,5 +1,13 @@
+from __future__ import annotations
+
+from typing import Literal
+
 from unstructured.partition.auto import partition
 from unstructured.partition.utils.constants import PartitionStrategy
+from unstructured.cleaners.core import group_broken_paragraphs
+from unstructured.cleaners.core import clean
+
+from core.cv_structures import StructuredCV
 
 
 def read_cv_from_file():
@@ -7,22 +15,58 @@ def read_cv_from_file():
     pass
 
 
-def read_cv_from_path(cv_path):
+def read_cv_from_path(cv_path: str) -> list[str]:
     # convert cv into a well-structured document
     elements = partition(
         filename=cv_path,
         content_type="application/pdf",
-        strategy=PartitionStrategy.OCR_ONLY,
+        strategy=PartitionStrategy.HI_RES,
+        paragraph_grouper=group_broken_paragraphs,
     )
-    print("\n\n".join([str(el) for el in elements]))
+
+    clean_elements = [
+        clean(
+            str(e),
+            extra_whitespace=True,
+            dashes=True,
+            bullets=True,
+            trailing_punctuation=True,
+            lowercase=True,
+        )
+        for e in elements
+    ]
+
+    return clean_elements
 
 
-def split_cv():
-    # extract all the covered points of interest and store them in a single blob
+def detect_header() -> None | Literal["work", "education", "project"]:
+    # check if chunk of text is a header or not
     pass
 
 
-def section_classifier():
-    # classify what kind of section the provided chunk of text is
-    # input can be either education, project, experience etc
+def verify_content_positioning() -> bool:
+    # check if chunk of text belongs to the current section
+    pass
+
+
+def regroup_cv(text_chunks: list[str]) -> list[str]:
+    # group extracted CV slices into better-organised fragments
+    # coagulate chunks until a header is encountered
+    # with each coagulation, also check if the text chunk belongs to the current section
+    pass
+
+
+# couple different approaches for section classification are feasible
+# - use AI to split into large sections: EXPERIENCE, EDUCATION ETC
+#   this would allow us to split the entire document by just detecting headers
+#   we would then have to split the individual entries into each section
+# - use AI to sort each individual entry into appropriate section
+#   simpler, but likely more lossy, as less context is being used
+# I'm going with the first one, but will see where it goes in the coming commits
+
+
+def process_cv(cv_path: str) -> StructuredCV:
+    raw_cv_chunks = read_cv_from_path(cv_path)
+    grouped_chunks = regroup_cv(raw_cv_chunks)
+
     pass
--- test_cv.py
+++ test_cv.py
@@ -1,5 +1,6 @@
 from core.cv_tools import read_cv_from_path
 
 # read CV
-read_cv_from_path("static/data-analyst.pdf")
-read_cv_from_path("static/senior-software-developer.pdf")
+raw_cv = read_cv_from_path("static/senior-software-developer.pdf")
+
+print("\n\nNEXT SECTION\n\n".join([str(el) for el in raw_cv]))
