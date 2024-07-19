cd6d7df76ddde5f4465def7b4aa23e59566c749a
LatekVon <latkaignacy@gmail.com>
2024-07-20 01:39:07 +0200

    complete individual section extraction functions

--- core/cv_extractors.py
+++ core/cv_extractors.py
@@ -1,3 +1,6 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_experimental.llms.ollama_functions import OllamaFunctions
+
 from core.cv_structures import (
     ExtractedProject,
     ExtractedDegree,
@@ -7,6 +10,24 @@ from core.cv_structures import (
     ExtractedOtherSearchable,
 )
 
+extraction_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are data extractor. "
+            "You are provided with distinct sections of CVs, "
+            "you are to extract the specified data out of them. "
+            "You are provided with a clear task and topic, "
+            "your job is to strictly follow the instructions. ",
+        ),
+        (
+            "user",
+            "Current section: ```{section}``` Data: ```{data}```",
+        ),
+    ]
+)
+
+functional_llm = OllamaFunctions(model="internlm2", format="json")
 
 # Either:
 # - ai regroups chunks of text into structured list jsons
@@ -18,25 +39,43 @@ from core.cv_structures import (
 # in all of the following cases, text is guaranteed to provide the extraction goal
 
 
-def extract_education(text: str) -> list[ExtractedDegree]:
-    pass
+def extract_education(text: str) -> ExtractedDegree:
+    structured_llm = functional_llm.with_structured_output(ExtractedDegree)
+    workflow = extraction_prompt | structured_llm
+    result = workflow.invoke({"data": text, "section": "education"})
+    return result
 
 
-def extract_commercial_experience(text: str) -> list[ExtractedRole]:
-    pass
+def extract_commercial_experience(text: str) -> ExtractedRole:
+    structured_llm = functional_llm.with_structured_output(ExtractedRole)
+    workflow = extraction_prompt | structured_llm
+    result = workflow.invoke({"data": text, "section": "commercial_experience"})
+    return result
 
 
-def extract_private_experience(text: str) -> list[ExtractedProject]:
-    pass
+def extract_private_experience(text: str) -> ExtractedProject:
+    structured_llm = functional_llm.with_structured_output(ExtractedProject)
+    workflow = extraction_prompt | structured_llm
+    result = workflow.invoke({"data": text, "section": "private_experience"})
+    return result
 
 
-def extract_websites(text: str) -> list[ExtractedWebsite]:
-    pass
+def extract_websites(text: str) -> ExtractedWebsite:
+    structured_llm = functional_llm.with_structured_output(ExtractedWebsite)
+    workflow = extraction_prompt | structured_llm
+    result = workflow.invoke({"data": text, "section": "websites"})
+    return result
 
 
-def extract_social_profiles(text: str) -> list[ExtractedSocialProfile]:
-    pass
+def extract_social_profiles(text: str) -> ExtractedSocialProfile:
+    structured_llm = functional_llm.with_structured_output(ExtractedSocialProfile)
+    workflow = extraction_prompt | structured_llm
+    result = workflow.invoke({"data": text, "section": "social_profiles"})
+    return result
 
 
-def extract_searchable_data(text: str) -> list[ExtractedOtherSearchable]:
-    pass
+def extract_searchable_data(text: str) -> ExtractedOtherSearchable:
+    structured_llm = functional_llm.with_structured_output(ExtractedOtherSearchable)
+    workflow = extraction_prompt | structured_llm
+    result = workflow.invoke({"data": text, "section": "searchable_data"})
+    return result
